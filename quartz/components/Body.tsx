8b3f83ce243b1cb902df4bc2b40823913df27a22
joaosour <81868077+joaosour@users.noreply.github.com>
2025-10-19 09:50:58 -0300

    Update Body.tsx para link external

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -1,5 +1,7 @@
 // @ts-ignore
 import clipboardScript from "./scripts/clipboard.inline"
+// @ts-ignore
+import openLinksScript from "./scripts/openLinks.inline"
 import clipboardStyle from "./styles/clipboard.scss"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
@@ -7,7 +9,8 @@ const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
   return <div id="quartz-body">{children}</div>
 }
 
-Body.afterDOMLoaded = clipboardScript
+// Executa ambos após o DOM carregar
+Body.afterDOMLoaded = clipboardScript + openLinksScript
 Body.css = clipboardStyle
 
 export default (() => Body) satisfies QuartzComponentConstructor
