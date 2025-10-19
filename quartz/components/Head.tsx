52dc058b993717e894575b868984d14ff48c2ef8
joaosour <joaovictorsousa45@gmail.com>
2025-10-19 20:43:40 -0300

    Quartz sync: Oct 19, 2025, 8:43 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -53,7 +53,7 @@ export default (() => {
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
         
 
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes" />
 
         <meta name="og:site_name" content={cfg.pageTitle}></meta>
         <meta property="og:title" content={title} />
